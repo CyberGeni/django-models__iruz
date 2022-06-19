de44dd6a9b1caae680387083964bb777d63f9f2c
CyberGeni <70205370+CyberGeni@users.noreply.github.com>
2022-06-19 15:52:11 +0100

    made migrations

--- I4G00899311P/blog/models.py
+++ I4G00899311P/blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
-
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField()
     created_date = models.DateTimeField(auto_now_add=True)
     published_date = models.DateTimeField(blank=True, null=True)
